198f197239e642e31b8d8e0198e15e4a65d91082
Jason Moy <jasonmoy28@gmail.com>
2021-04-07 20:15:37 +0800

    type: <feat> action: <added long_summary and changed bruceR summary. Removed the use of quos(). Added citations. >

--- R/cfa_summary.R
+++ R/cfa_summary.R
@@ -1,45 +1,80 @@
-#' CFA summary
+#' Confirmatory Factor Analysis
 #'
-#' The function is a wrapper of the lavaan::cfa. By default, it fits a lavaan model based on the data by treating everything columns measure a single latent variable.
+#' The function fits a CFA model using the lavaan::cfa (Rosseel, 2012) function. In addition to passing an explicit lavaan model, users can fit a unifactor CFA using the `items` argument. Moreoever, users can request 3 types of summary for the model (see below for options). 
 #'
-#' @param model explicit lavaan model. use lavaan syntax. If model is not explictly specified, the function will run a CFA assuming all columns belong to one latent variable.
-#' @param data dataframe
+#' @param data data frame
+#' @param model explicit lavaan model. Either the `model` argument or the `items` argument must be specified.
 #' @param group optional character. default is NULL. the nested variable for multilevel dataset (e.g., Country)
-#' @param items vector or quos(). default to NULL. create a lavaan formula if the formula is not explicitly set in model. if you want to use tidyselect syntax, wrap it in quos() (e.g. quos(contains('Q'))). This argument will be ignored if model is specified.
-#' @param summary_item vector. default is cfi, rmsea, and tli. See lavaan for more option
+#' @param items vector of tidyselect syntax or helpers. default to NULL if the model is specified. The argument will be ignored if the model is explicitly specified.
+#' @param summary_item vector of fit indices. Default is cfi, rmsea, tli, and srmr. 
 #' @param ordered logical. default is F. If it is set to T, lavaan will treat it as a ordinal variable and use DWLS instead of ML
-#' @param return_result character. default is 'model'. You can set it to 'short_summary' for fit measures
-#' @param quite default as F. If set to true, it will not print the statement that tells you what the lavaan model is going to run.
-#' @param group_partial items for partial equivalence. The form should be c('DV =~ item1', 'DV =~ item2').
+#' @param return_result Default is model. Options are 'model' (lavaan model), 'short_summary' (fit index summary only), 'long_summary' (lavaan full summary), or 'bruceR_summary' (uses the bruceR::CFA (Bao, 2021) function. Require the bruceR package. Please note that bruceR package may take a long time to install due to its dependencies)
+#' @param quite default as F. If set to true, it will not print the running model statement. 
+#' @param group_partial Items for partial equivalence. The form should be c('DV =~ item1', 'DV =~ item2').
 #'
-#' @return
+#' @references 
+#' 
+#' Bao, H.-W.-S. (2021). bruceR: Broadly useful convenient and efficient R functions. R package version 0.6.0. https://CRAN.R-project.org/package=bruceR 
+#' 
+#' Rosseel Y (2012). lavaan: An R Package for Structural Equation Modeling. Journal of Statistical Software, 48(2), 1–36. https://www.jstatsoft.org/v48/i02/.
+#' 
 #' @export
-#'
 #' @examples
-#'
-cfa_summary = function(model = NULL,
-                       data,
+#' # Fitting a CFA model by passing explicit lavaan model. 
+#' cfa_summary(model = 'visual  =~ x1 + x2 + x3
+#'                      textual =~ x4 + x5 + x6
+#'                      speed   =~ x7 + x8 + x9 ',
+#'             data = lavaan::HolzingerSwineford1939)
+#'             
+#' # Fitting a unifactor CFA model by passing items.
+#' cfa_summary(items = x1:x3,
+#'             data = lavaan::HolzingerSwineford1939)
+#' 
+#' 
+cfa_summary = function(data,
+                       model = NULL,
                        group = NULL,
                        items = NULL,
-                       summary_item = c('cfi', 'rmsea', 'tli'),
+                       summary_item = c('cfi', 'rmsea', 'tli','srmr'),
                        ordered = F,
                        return_result = 'model',
                        quite = F,
                        group_partial = NULL) {
-
+  
+  items = ggplot2::enquo(items)
+  
   if (is.null(model)) {
-    cfa_data = data %>% dplyr::select(!!!items, !!!group)
-    cfa_items = data %>% dplyr::select(!!!items) %>% names(.)
+    cfa_items = data %>% dplyr::select(!!items) %>% names(.)
     model = paste('DV =~', paste(cfa_items, collapse = ' + '))
   }
-
-  cfa_data = data
+  
+  # Print statement
   if (quite == F) {
     print(paste('Computing CFA using:',model))
   }
+  
+  # for long summary result, run CFA using bruceR
+  if(return_result == 'bruceR_summary') {
+    if (!is.null(group)) {
+      warning('Group variable is ignored. Multilevel CFA is only supported with returning model or short_summary')
+    }
+    check_package = requireNamespace('bruceR')
+    if (check_package == F) {
+      response = readline('Install bruceR package? It may take a long time to install. Enter Y/N ')
+      if (stringr::str_to_upper(response) == 'Y') {
+        install.packages('bruceR')
+        bruceR::CFA(data = data, model = model)
+      } else{
+        print('Installation Halted. Please do not pass "bruceR_summary" to the return_result argument')
+      } 
+    } else{
+    bruceR::CFA(data = data, model = model)
+    }
+  }
+
   cfa_model = lavaan::cfa(
     model = model,
-    data = cfa_data,
+    data = data,
     group = group,
     ordered = ordered,
     group.partial = group_partial
@@ -52,7 +87,7 @@ cfa_summary = function(model = NULL,
   } else if (return_result == 'short_summary') {
     cfa_short_summary = lavaan::fitMeasures(cfa_model)[summary_item]
     return(cfa_short_summary)
-  } else if(return_result == 'long_summary') {
-    summary(cfa_model,fit.measures = T, standardized = T)
+  } else if(return_result == 'long_summary'){
+    lavaan::summary(cfa_model, fit.measure = T, standardized = T)
   }
 }
